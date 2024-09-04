5154578399e894aa6d4864e26bf84716b6106d4d
mehh000 <mdhimal2040@gmail.com>
2024-09-05 00:43:35 +0600

    run buld and es

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Loading() {
   return (
     <div className="flex items-center justify-center w-full h-svh">
-      <Image src='/loading.gif' width={100} height={200} />
+      <Image src='/loading.gif' width={100} height={200} alt='loading' />
     </div>
   )
 }
